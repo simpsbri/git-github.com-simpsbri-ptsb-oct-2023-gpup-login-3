a801abb74a5b96836e7321fa07ba08a6b563998e
simpsbri <simpsbri@gmail.com>
2024-01-11 19:35:48 -0600

    push cart.js update and style error correction
    undefined

--- cart.js
+++ cart.js
@@ -1,5 +1,5 @@
 let shoppingCart = [];
-
+let cartItems = '';
 // Function to add item to the shopping cart
 function addToCart(itemName, itemPrice) {
   // Create a new object with item name and price
@@ -17,8 +17,6 @@ function addToCart(itemName, itemPrice) {
 
 // Function to display the shopping cart items
 function displayCart() {
-  let cartItems = '';
-
   // Loop through the shopping cart array and display the items
   for (let i = 0; i < shoppingCart.length; i++) {
     cartItems += shoppingCart[i].name + ' - $' + shoppingCart[i].price + '<br>';
